96cbf29a93b5709410e6c8dfee412d1242b35163
Abdur Rahman <qaziabdurrahman12@gmail.com>
2025-08-21 23:27:26 +0500

    updated code

--- src/index.js
+++ src/index.js
@@ -73,7 +73,7 @@ app.set("io", io);
 // === Middleware ===
 app.use(
   cors({
-    origin: ["http://admin.virsme.com", "http://admin.innand.com", "http://apis.innand.com", "http://employee.virsme.com", "http://hr.virsme.com", "http://localhost:8080", "http://localhost:8081"],
+    origin: ["http://admin.virsme.com", "http://admin.innand.com", "http://apis.innand.com", "http://employee.virsme.com", "http://hr.virsme.com", "http://localhost:8080", "http://innand.com"],
     credentials: true, // if you need cookies/auth
   })
 );
